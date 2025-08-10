8f2dfa0f714c93272b319b0c46669a08d454a388
Ali Tourani <a.tourani1991@gmail.com>
2025-08-10 20:21:03 +0200

    [feat] - add movielens data

--- movifex/datasets/utils.py
+++ movifex/datasets/utils.py
@@ -52,3 +52,31 @@ def applyInteractionLimits(data: pd.DataFrame, minInteraction: int, maxInteracti
     usersToKeep = interactionCount[(interactionCount >= minInteraction) & (interactionCount <= maxInteraction)].index
     # Return the filtered DataFrame
     return data[data[userColumnName].isin(usersToKeep)]
+
+def applyKcore(df, k):
+    """
+    Apply k-core filtering to the dataset.
+    This function filters the DataFrame to retain only users and items
+    that have at least k interactions.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The DataFrame containing user-item interactions.
+    k : int
+        The minimum number of interactions required for users and items to be retained.
+
+    Returns
+    -------
+    pd.DataFrame
+        The filtered DataFrame containing only users and items with at least k interactions.
+    """
+    changed = True
+    while changed:
+        before = len(df)
+        vc = df.user_id.value_counts()
+        df = df[df.user_id.isin(vc[vc >= k].index)]
+        vc = df.item_id.value_counts()
+        df = df[df.item_id.isin(vc[vc >= k].index)]
+        changed = len(df) < before
+    return df
