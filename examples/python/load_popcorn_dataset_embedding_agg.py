255d249143bc332247a9c8bd63858424e50b2077
Ali Tourani <a.tourani1991@gmail.com>
2025-09-20 20:20:40 +0200

    [feat] - finalize loader for pocorn-dataset

--- examples/python/load_popcorn_dataset_embedding_agg.py
+++ examples/python/load_popcorn_dataset_embedding_agg.py
@@ -3,8 +3,11 @@
 from popcorn.utils import readConfigs
 from popcorn.datasets.popcorn.utils import RAW_DATA_URL
 from popcorn.datasets.popcorn.helper_embedding_agg import (
-    generateAggEmbeddingUrl,
-    fetchAggregatedFeatures,
+    loadAggEmbeddings,
+    fetchMovieAggEmbeddings,
+    generateAllAggEmbeddingUrls,
+    generateMovieAggEmbeddingUrl,
+    generateMoviesAggEmbeddingUrls,
 )
 
 
@@ -29,10 +32,44 @@ def main():
     print(f"\n[Util-1] Generating a sample address to aggregated features ...")
     givenMovieId = 6
     givenCnn, givenEmbedding = cnns[0], aggEmbeddings[0]
-    aggEmbeddingUrl = generateAggEmbeddingUrl(givenEmbedding, givenCnn, givenMovieId)
+    aggEmbeddingUrl = generateMovieAggEmbeddingUrl(givenEmbedding, givenCnn, givenMovieId)
     print(
         f"- URL for aggregated features of movie '#{givenMovieId}' extracted by CNN '{givenCnn}' from source '{givenEmbedding}': {aggEmbeddingUrl}"
     )
+    # [Util-2] Test fetching aggregated features of a movie
+    print(f"\n[Util-2] Fetching aggregated features of a movie ...")
+    aggEmbeddingList = fetchMovieAggEmbeddings(givenEmbedding, givenCnn, givenMovieId)
+    print(f"- Fetched {len(aggEmbeddingList)} aggregated features! Sample embeddings:")
+    print(f"-- 'Max': {aggEmbeddingList[0]['Max'][:5]}")
+    print(f"-- 'Mean': {aggEmbeddingList[0]['Mean'][:5]}")
+    # [Util-3] Test generating all addresses to aggregated features
+    givenMovieIds = [6, 50]
+    aggEmbeddings = ["full_movies_agg", "movie_trailers_agg"]
+    print(f"\n[Util-3] Generating all addresses to aggregated features for movies {givenMovieIds} extracted by CNNs {cnns} from sources {aggEmbeddings} ...")
+    allAggEmbeddingUrls = generateMoviesAggEmbeddingUrls(aggEmbeddings, cnns, givenMovieIds)
+    print(f"- Generated {len(allAggEmbeddingUrls)} variants of aggregated feature addresses!")
+    print(f"-- Keys: {list(allAggEmbeddingUrls.keys())}")
+    print(f"-- Sample addresses for '{aggEmbeddings[0]}' and '{cnns[0]}': {allAggEmbeddingUrls[aggEmbeddings[0]][cnns[0]]}")
+    # [Util-4] Test generating all addresses to aggregated features based on the configuration file
+    print(f"\n[Util-4] Generating all addresses to aggregated features based on the configuration file ...")
+    aggEmbeddingUrlDict = generateAllAggEmbeddingUrls(configs)
+    if not aggEmbeddingUrlDict:
+        print("- Error in generating the addresses! Continuing ...")
+    else:
+        print(f"- Generated {len(aggEmbeddingUrlDict)} variants of aggregated feature addresses!")
+        print(f"- Sample addresses for '{aggEmbeddings[0]}' and '{cnns[0]}': {aggEmbeddingUrlDict[aggEmbeddings[0]][cnns[0]][:2]}")
+    # [Util-5] Test loading aggregated features into a DataFrame
+    print(f"\n[Util-5] Loading aggregated features into a DataFrame ...")
+    if not aggEmbeddingUrlDict:
+        print("- Error in loading the aggregated features! Skipping ...")
+    else:
+        # Take a few samples of the generated addresses
+        aggEmbeddingUrlList = aggEmbeddingUrlDict[aggEmbeddings[0]][cnns[0]][:5]
+        # Load aggregated features into a DataFrame
+        dfAggEmbedsMax, dfAggEmbedsMean = loadAggEmbeddings(aggEmbeddingUrlList)
+        print(f"- Loaded {len(dfAggEmbedsMax)} sample records of aggregated features! Check the first 3 records:")
+        print(f"- The loaded DataFrame (Max):\n{dfAggEmbedsMax.head(3)}")
+        print(f"- The loaded DataFrame (Mean):\n{dfAggEmbedsMean.head(3)}")
     # Stop
     print("\nStopping 'Popcorn'!")
 
--- popcorn/datasets/popcorn/helper_embedding_agg.py
+++ popcorn/datasets/popcorn/helper_embedding_agg.py
@@ -3,12 +3,13 @@ from popcorn.utils import loadJsonFromUrl
 from popcorn.datasets.popcorn.helper_metadata import fetchAllMovieIds
 from popcorn.datasets.popcorn.utils import (
     RAW_DATA_URL,
+    METADATA_URL,
     isValidCNN,
     isValidAggEmbeddingSource,
 )
 
 
-def generateAggEmbeddingUrl(embedding: str, cnn: str, movieId: int) -> str:
+def generateMovieAggEmbeddingUrl(embedding: str, cnn: str, movieId: int) -> str:
     """
     Generates the address of an embedding packet file based on the given parameters.
 
@@ -41,7 +42,105 @@ def generateAggEmbeddingUrl(embedding: str, cnn: str, movieId: int) -> str:
     return aggEmbeddingUrl
 
 
-def fetchAggregatedFeatures(embedding: str, cnn: str, movieId: int) -> list:
+def generateMoviesAggEmbeddingUrls(
+    embeddings: list, cnns: list, movieIds: list
+) -> dict:
+    """
+    Generates all addresses of aggregated embedding packet files based on the given parameters.
+
+    Parameters
+    ----------
+    embeddings: list
+        A list of embedding types (e.g., ["full_movies"]).
+    cnns: list
+        A list of CNN models (e.g., ["incp3", "vgg19"]).
+    movieIds: list
+        A list of movie IDs (e.g., [1, 2, 3]).
+
+    Returns
+    -------
+    aggEmbeddingUrlDict: dict
+        A dictionary containing lists of generated addresses of the embedding packet files.
+    """
+    # Variables
+    aggEmbeddingUrlDict = {}
+    # Check arguments
+    if not embeddings or not cnns or not movieIds:
+        print(f"- [Error] No valid input lists provided! Stopping ...")
+        return aggEmbeddingUrlDict
+    isValidCnn = all(isValidCNN(cnn) for cnn in cnns)
+    isValidEmbedding = all(isValidAggEmbeddingSource(emb) for emb in embeddings)
+    if not isValidCnn or not isValidEmbedding:
+        print(f"- [Error] Invalid CNN or embedding type found! Stopping ...")
+        return aggEmbeddingUrlDict
+    # Fill the dictionary
+    for embedding in embeddings:
+        aggEmbeddingUrlDict[embedding] = {}
+        for cnn in cnns:
+            aggEmbeddingUrlDict[embedding][cnn] = []
+    # Loop over all movies
+    for movieId in movieIds:
+        # Format movie ID
+        if not isinstance(movieId, int):
+            movieId = int(movieId)
+        movieId = f"{int(movieId):010d}"
+        # Loop over all feature models
+        for cnn in cnns:
+            # Loop over all aggregated feature sources
+            for embedding in embeddings:
+                # Generate address
+                address = RAW_DATA_URL + f"{embedding}/{cnn}/{movieId}.json"
+                aggEmbeddingUrlDict[embedding][cnn].append(address)
+    # Return
+    return aggEmbeddingUrlDict
+
+
+def generateAllAggEmbeddingUrls(configs: dict) -> dict:
+    """
+    Fetches all aggregated features of movies from the dataset based on the given configuration.
+
+    Parameters:
+    -----------
+    configs: dict
+        A dictionary containing the dataset configuration.
+
+    """
+    # Variables
+    aggEmbeddingUrlDict = {}
+    # Read configurations
+    cnns = configs["datasets"]["multimodal"]["popcorn"]["cnns"]
+    embeddings = configs["datasets"]["multimodal"]["popcorn"]["agg_embedding_sources"]
+    # Fetch JSON metadata from the URL
+    print(f"- Fetching URL from '{METADATA_URL}' ...")
+    jsonData = loadJsonFromUrl(METADATA_URL)
+    if jsonData is None:
+        print("- Error in loading the Popcorn dataset metadata! Exiting ...")
+        return aggEmbeddingUrlDict
+    # Fetch all movie IDs
+    print(f"- Fetching all movie IDs ...")
+    movieIds = fetchAllMovieIds(jsonData)
+    # Generating a list of addresses to fetch the aggregated features
+    print(
+        f"- Generating a list of addresses to fetch the aggregated features of all movies (CNNs: {cnns}, Embeddings: {embeddings}) ..."
+    )
+    aggEmbeddingUrlDict = generateMoviesAggEmbeddingUrls(embeddings, cnns, movieIds)
+    if not aggEmbeddingUrlDict:
+        print("- Error in generating the addresses! Exiting ...")
+        return {}
+    # Count all members of the generated addresses
+    count = (
+        len(aggEmbeddingUrlDict)
+        * len(aggEmbeddingUrlDict[embeddings[0]])
+        * len(aggEmbeddingUrlDict[embeddings[0]][cnns[0]])
+    )
+    print(
+        f"- Generated {count} aggregated feature addresses, e.g., {aggEmbeddingUrlDict['full_movies_agg']['incp3'][0]} ..."
+    )
+    # Return
+    return aggEmbeddingUrlDict
+
+
+def fetchMovieAggEmbeddings(embedding: str, cnn: str, movieId: int) -> list:
     """
     Fetches all aggregated features of a movie from the given embedding type and CNN model.
 
@@ -56,147 +155,82 @@ def fetchAggregatedFeatures(embedding: str, cnn: str, movieId: int) -> list:
 
     Returns
     -------
-    movieAggEmbeddings: list
+    aggEmbeddingList: list
         A list of all aggregated features of the movie.
     """
     # Variables
-    movieAggEmbeddings = []
+    aggEmbeddingList = []
+    # Check arguments
+    isValidCnn = isValidCNN(cnn)
+    isValidEmbedding = isValidAggEmbeddingSource(embedding)
+    if not isValidCnn or not isValidEmbedding:
+        return aggEmbeddingList
     # Generate packet address
-    aggFeatureAddress = generateAggEmbeddingUrl(embedding, cnn, movieId)
+    aggEmbeddingUrl = generateMovieAggEmbeddingUrl(embedding, cnn, movieId)
     # Check if address is valid
-    if aggFeatureAddress == "":
+    if aggEmbeddingUrl == "":
         print(f"- [Error] No valid address generated! Stopping ...")
-        return movieAggEmbeddings
+        return aggEmbeddingList
     # Fetch JSON data
-    movieAggEmbeddings = loadJsonFromUrl(aggFeatureAddress)
+    aggEmbeddingList = loadJsonFromUrl(aggEmbeddingUrl)
     # Return
-    return movieAggEmbeddings
+    return aggEmbeddingList
 
 
-def getAllAggFeatureUrls(featureModels: list, movieIds: list, aggFeatureSources: list):
+def loadAggEmbeddings(aggEmbeddingUrlList: list) -> tuple:
     """
-    Generates a list of addresses for all aggregated features of movies in the dataset.
+    Loads aggregated features from a list of URLs into two DataFrames (Max and Mean).
 
-    Parameters:
-        datasetUrl (str): The base URL of the dataset.
-        featureModels (list): A list of feature models used for feature extraction.
-        movieIds (list): A list of all movie IDs.
-        aggFeatureSources (list): A list of aggregated feature sources.
+    Parameters
+    ----------
+    aggEmbeddingUrlList: list
+        A list of URLs pointing to the aggregated feature JSON files.
 
-    Returns:
-        aggFeatureAddresses (dict): A dictionary of all aggregated feature addresses.
-    """
-    # Variables
-    aggFeatureAddresses = {
-        "full_movies_agg": {"incp3": [], "vgg19": []},
-        "movie_shots_agg": {"incp3": [], "vgg19": []},
-        "movie_trailers_agg": {"incp3": [], "vgg19": []},
-    }
-    # Loop over all movies
-    for movieId in movieIds:
-        if not isinstance(movieId, int):
-            continue
-        # Loop over all feature models
-        for featureModel in featureModels:
-            # Loop over all aggregated feature sources
-            for aggFeatureSource in aggFeatureSources:
-                # Generate address
-                address = (
-                    RAW_DATA_URL + f"{aggFeatureSource}/{featureModel}/{movieId}.json"
-                )
-                aggFeatureAddresses[aggFeatureSource][featureModel].append(address)
-    # Return
-    return aggFeatureAddresses
-
-
-def generatedAggFeatureAddresses(cfgDatasets: dict):
-    """
-    Fetches all aggregated features of movies from the dataset and combines them into a DataFrame.
-
-    Parameters:
-        datasetUrl (str): The base URL of the dataset.
-        embedding (str): The feature type (e.g., "audio", "visual").
-        cnn (str): The model used for feature extraction.
-        movieId (int): The ID of the movie.
-
-    Returns:
-        moviePackets (list): A list of all packets of the movie.
-    """
-    # Variables
-    rawFilesUrl = cfgDatasets["path_raw"]
-    featureModels = cfgDatasets["feature_models"]
-    datasetMetadataUrl = cfgDatasets["path_metadata"]
-    aggFeatureSources = cfgDatasets["agg_feature_sources"]
-    # Fetch JSON metadata from the URL
-    print(f"- Fetching URL from '{datasetMetadataUrl}' ...")
-    jsonData = loadJsonFromUrl(datasetMetadataUrl)
-    # Fetch all movie IDs
-    print(f"- Fetching all movie IDs ...")
-    movieIds = fetchAllMovieIds(jsonData)
-    print(f"- Found {len(movieIds)} movie IDs ...")
-    # Generating a list of addresses to fetch the aggregated features
-    print(f"- Generating a list of addresses to fetch the aggregated features ...")
-    aggFeatureAddresses = allAggregatedFeatureAddresses(
-        rawFilesUrl, featureModels, movieIds, aggFeatureSources
-    )
-    # Count all members of aggFeatureAddresses
-    numberOfGeneratedAddresses = (
-        len(aggFeatureAddresses)
-        * len(aggFeatureAddresses["full_movies_agg"])
-        * len(aggFeatureAddresses["full_movies_agg"]["incp3"])
-    )
-    print(
-        f"- Generated {numberOfGeneratedAddresses} aggregated feature addresses, e.g., {aggFeatureAddresses['full_movies_agg']['incp3'][0]} ..."
-    )
-    # Return
-    return aggFeatureAddresses
-
-
-def loadAggregatedFeaturesIntoDataFrame(aggFeatureAddresses: list):
-    """
-    Loads all aggregated features of movies from the dataset into a DataFrame.
-
-    Parameters:
-        aggFeatureAddresses (list): A list of all aggregated feature addresses.
-
-    Returns:
-        dfAggFeaturesMax (DataFrame): A DataFrame containing the maximum aggregated features.
-        dfAggFeaturesMean (DataFrame): A DataFrame containing the mean aggregated features.
+    Returns
+    -------
+    dfAggEmbedsMax: pd.DataFrame
+        A DataFrame containing the 'Max' aggregated features.
+    dfAggEmbedsMean: pd.DataFrame
+        A DataFrame containing the 'Mean' aggregated features.
     """
     # Variables
     counter = 0
-    dfAggFeaturesMax = pd.DataFrame()
-    dfAggFeaturesMean = pd.DataFrame()
+    dfAggEmbedsMax = pd.DataFrame()
+    dfAggEmbedsMean = pd.DataFrame()
+    # Check input
+    if not aggEmbeddingUrlList or not isinstance(aggEmbeddingUrlList, list):
+        print(f"- [Error] No valid list of addresses provided! Stopping ...")
+        return pd.DataFrame(), pd.DataFrame()
     # Loop over all addresses
-    for item in aggFeatureAddresses:
+    for url in aggEmbeddingUrlList:
         # Variables
-        itemId = item.split("/")[-1].split(".")[0]
+        itemId = url.split("/")[-1].split(".")[0]
         # Fetch the JSON data from the URL
-        jsonData = loadJsonFromUrl(item)
+        jsonData = loadJsonFromUrl(url)
         aggFeatMax, aggFeatMean = jsonData[0]["Max"], jsonData[0]["Mean"]
         # Convert the list to a string, like "0.1,0.2,0.3"
         aggFeatMax = ",".join(map(str, aggFeatMax))
         aggFeatMean = ",".join(map(str, aggFeatMean))
         # Append to the DataFrames
-        dfAggFeaturesMax = pd.concat(
+        dfAggEmbedsMax = pd.concat(
             [
-                dfAggFeaturesMax,
-                pd.DataFrame([{"itemId": int(itemId), "embedding": aggFeatMax}]),
+                dfAggEmbedsMax,
+                pd.DataFrame([{"item_id": int(itemId), "embedding": aggFeatMax}]),
             ],
             ignore_index=True,
         )
-        dfAggFeaturesMean = pd.concat(
+        dfAggEmbedsMean = pd.concat(
             [
-                dfAggFeaturesMean,
-                pd.DataFrame([{"itemId": int(itemId), "embedding": aggFeatMean}]),
+                dfAggEmbedsMean,
+                pd.DataFrame([{"item_id": int(itemId), "embedding": aggFeatMean}]),
             ],
             ignore_index=True,
         )
         # Better logging for the user
         counter += 1
-        if counter % 100 == 0:
+        if counter % 2 == 0:
             print(
-                f"- Loading aggregated features ({int(counter / len(aggFeatureAddresses) * 100)}%) ..."
+                f"- Loading aggregated features ({int(counter / len(aggEmbeddingUrlList) * 100)}%) ..."
             )
     # Return
-    return dfAggFeaturesMax, dfAggFeaturesMean
+    return dfAggEmbedsMax, dfAggEmbedsMean
